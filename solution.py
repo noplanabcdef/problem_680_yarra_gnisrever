2eb6b0ce9fb5ea7b888ae5c7e95649dabceed7f8
noplanabcdef <blooketmain@gmail.com>
2025-05-16 16:56:54 +0800

    test solution.py

--- solution.py
+++ solution.py
@@ -1,1 +1,10 @@
-
+def fast_doubling(n, mod):
+    if n == 0:
+        return (0, 1)
+    a, b = fast_doubling(n // 2, mod)
+    c = a * (2 * b - a) % mod
+    d = (a * a + b * b) % mod
+    if n % 2 == 0:
+        return (c, d)
+    else:
+        return (d, (c + d) % mod)
